388923a99c540b1e3bbec704d1975a06927f3a90
unknown <1302534@MC10810.uad.ac.uk>
2017-09-28 17:05:50 +0100

    touch hatred

---
+++ Assets/Resources/Scripts/Touch/TouchPadManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPadManager : MonoBehaviour, ITouchReceiver {
+
+
+	BoxCollider collider;
+	ITouchReceiver touchReceiver;
+
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	private GameObject selectedPlane;
+
+	public void setSelectedPlane(GameObject gameObject){
+	
+		selectedPlane = gameObject;
+		touchReceiver = selectedPlane.GetComponent<ITouchReceiver> ();
+
+	}
+
+	public void OnTouchUp(Vector3 point)
+	{
+		Debug.Log ("Calling this shit");
+		touchReceiver.OnTouchUp (point);
+
+	}
+
+	public void OnTouchDown(Vector3 point)
+	{
+	}
+
+	public void OnTouchMove(Vector3 point)
+	{
+
+		touchReceiver.OnTouchMove (point);
+
+	}
+
+	public void OnTouchStay(Vector3 point)
+	{
+
+		touchReceiver.OnTouchStay (point);
+
+	}
+
+	public void OnTouchExit(Vector3 point)
+	{
+
+		touchReceiver.OnTouchExit (point);
+
+	}
+
+
+}
