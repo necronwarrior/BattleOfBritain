d9e3918ee8db0001f7a2840bfee6b6712e125d77
unknown <1704603@MC10780.uad.ac.uk>
2017-09-28 15:09:39 +0100

    Touch gestures correctly detected with the mouse and the Unity Editor

---
+++ Assets/Resources/Scripts/Touch/DummyTouchReceiver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyTouchReceiver : MonoBehaviour, ITouchReceiver
+{
+
+    public void OnTouchUp(Vector3 point)
+    {
+        Debug.Log("OnTouchUp called, point: " + point);
+    }
+
+    public void OnTouchDown(Vector3 point)
+    {
+        Debug.Log("OnTouchDown called, point: " + point);
+    }
+
+    public void OnTouchMove(Vector3 point)
+    {
+        Debug.Log("OnTouchMove called, point: " + point);
+    }
+
+    public void OnTouchStay(Vector3 point)
+    {
+        Debug.Log("OnTouchStay called, point: " + point);
+    }
+
+    public void OnTouchExit(Vector3 point)
+    {
+        Debug.Log("OnTouchExit called, point: " + point);
+    }
+
+
+}
--- Assets/Resources/Scripts/Touch/TouchInput.cs
+++ Assets/Resources/Scripts/Touch/TouchInput.cs
@@ -6,7 +6,21 @@ public class TouchInput : MonoBehaviour {
 
 	public LayerMask touchInputMask;
 
-	private List<GameObject> touchList = new List<GameObject> ();
+#if UNITY_EDITOR
+
+    /*
+        It seems to detect move and stay actions correctly
+        with the 0.0f value so we'll just leave it like that
+        for now since it is just for testing purposes in the
+        Unity Editor.
+    */
+    public float mouseMoveThreshold = 0.0f;
+    private Vector3 lastMousePosition;
+
+#endif
+
+
+    private List<GameObject> touchList = new List<GameObject> ();
 	private GameObject[] touchListOld;
 	private RaycastHit touchHit;
 
@@ -27,22 +41,34 @@ public class TouchInput : MonoBehaviour {
 			touchList.CopyTo (touchListOld);
 			touchList.Clear ();
 
-			foreach (Touch touch in Input.touches) {
+
+#if UNITY_EDITOR
+
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#else
+                /*
+                This is in case we want to check for multiple touches
+                from the player. That is, multiple fingers at the same
+                time (only one possible interpretation to that sentence ok?)
+                */
+                foreach (Touch touch in Input.touches) {                
 				Ray ray = Camera.main.ScreenPointToRay (touch.position);
-				RaycastHit touchHit;
+#endif
+
+                RaycastHit touchHit;
 
 				if (Physics.Raycast (ray, out touchHit, 1000, touchInputMask)) {
 
                     /*
-                        We can safely send the message to the gameobject since
-                        it will just call every method with that name inside every
-                        component in the game object. We need to be careful with not naming some
-                        other functions like these. :D
-                    
+                    We can safely send the message to the gameobject since
+                    it will just call every method with that name inside every
+                    component in the game object. We need to be careful with not naming some
+                    other functions like these. :D
                     */
                     GameObject hitObject = touchHit.transform.gameObject;
 
-#if UNITY_EDITOR
+#if UNITY_EDITOR    //  BRANCH: TOUCH JUST BEGAN
 
                     if (Input.GetMouseButtonDown(0)) {
 #else
@@ -53,7 +79,8 @@ public class TouchInput : MonoBehaviour {
 
 					}else
 
-#if UNITY_EDITOR
+#if UNITY_EDITOR    //  BRANCH: TOUCH JUST FINISHED
+
                     if (Input.GetMouseButtonUp(0)) {
 #else
                     if (touch.phase == TouchPhase.Ended) {
@@ -62,17 +89,31 @@ public class TouchInput : MonoBehaviour {
 
 					}else
 
-#if UNITY_EDITOR
-                    if (Input.GetMouseButton(0)) {
+#if UNITY_EDITOR    //  BRANCH: THE USER IS TOUCHING AND DRAGGING
+
+                    if (Input.GetMouseButton(0) && (((lastMousePosition - Input.mousePosition).magnitude) > mouseMoveThreshold)) {
 #else
-					if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+					if (touch.phase == TouchPhase.Moved) {
 #endif
                         hitObject.SendMessage ("OnTouchMove", touchHit.point, SendMessageOptions.DontRequireReceiver);
 
 					}else
 
-#if UNITY_EDITOR
-                    if (Input.GetMouseButton(1)) {
+#if UNITY_EDITOR    //  BRANCH: THE USER IS TOUCHING AND NOT DRAGGING
+
+                    if (Input.GetMouseButton(0))
+                    {
+#else
+					if (touch.phase == TouchPhase.Stationary) {
+#endif
+                        hitObject.SendMessage("OnTouchStay", touchHit.point, SendMessageOptions.DontRequireReceiver);
+
+                    }
+                    else
+
+#if UNITY_EDITOR    //  BRANCH: SOMETHING NOT REALLY DESIRED HAPPENED AND WE HAVE TO CANCEL THE TOUCH
+
+                    if (Input.GetMouseButton(1)) {  // In Unity we simulate it with a right click.
 #else
 					if (touch.phase == TouchPhase.Canceled) {
 #endif
@@ -82,16 +123,22 @@ public class TouchInput : MonoBehaviour {
 				}
 			}
 
+            //In case we are not touching something we were previously touching
 			foreach (GameObject G in touchListOld) {
 				if (!touchList.Contains (G)) {
+                    /*
+                    I'm not sure if this should be "OnTouchExit" or "OnTouchUp". Both are likely to have 
+                    a similar funcionality on the final implementation of the component
+                    */
 					G.SendMessage ("OnTouchExit",touchHit.point, SendMessageOptions.DontRequireReceiver);
 				}
 			}
 
+            lastMousePosition = Input.mousePosition;
 
-		}//End of if(input)
+        }//End of if(input)
 
 
-	}//End of Update()
+    }//End of Update()
 
 }//End of Class
