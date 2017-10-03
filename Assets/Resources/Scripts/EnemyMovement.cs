7cdf0c30abf088b25a4dd678906343857b1665b4
unknown <1302534@MC10810.uad.ac.uk>
2017-10-03 15:17:42 +0100

    i did the map thing

--- Assets/Resources/Scripts/EnemyMovement.cs
+++ Assets/Resources/Scripts/EnemyMovement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
+	public Transform Startpos, Finishpos;
+	public float GoTime;
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = Startpos.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.timeSinceLevelLoad > GoTime) {
+			GetComponent<SplineInterpolator> ().enabled;
+		}
 	}
 }
