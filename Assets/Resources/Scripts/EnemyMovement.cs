5380db9cf9d4f37e75f30b59b21c1faa593af3e9
unknown <1704603@MC10780.uad.ac.uk>
2017-10-03 16:03:56 +0100

    Fixed merge error

--- Assets/Resources/Scripts/EnemyMovement.cs
+++ Assets/Resources/Scripts/EnemyMovement.cs
@@ -13,8 +13,8 @@ public class EnemyMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Time.timeSinceLevelLoad > GoTime) {
-			GetComponent<SplineController> ().SplineRoot = Parent;
-			GetComponent<SplineController> ().Duration = 8.0f;
+			//GetComponent<SplineController> ().SplineRoot = Parent;
+			//GetComponent<SplineController> ().Duration = 8.0f;
 		}
 	}
 }
